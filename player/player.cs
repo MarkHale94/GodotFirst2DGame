d1a2ee7fa8c4487c28500fdb4b8e15faafa32a07
Mark Hale <mark.hale94@gmail.com>
2023-10-24 16:30:17 -0500

    Creating the player character

---
+++ player/player.cs
@@ -0,0 +1,84 @@
+using Godot;
+using System;
+
+public partial class player : Area2D
+{
+	[Export] public int Speed { get; set; } = 400;
+	
+	public Vector2 ScreenSize;
+
+	[Signal]
+	public delegate void HitEventHandler();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		if (Input.IsActionPressed("MoveRight"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("MoveLeft"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("MoveDown"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("MoveUp"))
+		{
+			velocity.Y -= 1;
+		}
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+		
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		//Hide the player after being hit
+		Hide();
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+}
